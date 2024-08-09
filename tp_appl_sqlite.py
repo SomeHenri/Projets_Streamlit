0525244ee4373dccab6d8f9906b5a7ef09089c26
SOME Henri <nifago@gmail.com>
2024-08-09 15:06:22 +0000

    Ajout du fichier tp_appl_sqlite.py

--- tp_appl_sqlite.py
+++ tp_appl_sqlite.py
@@ -23,11 +23,11 @@ conn.commit()
 st.sidebar.title("Menu")
 
 # je veux un bouton radio dans la marge afin de selectionner la page que je veux
-page = st.sidebar.radio("Aller à", ["Liste des étudiants", "Ajouter des étudiants"])
+page = st.sidebar.radio("Go to", ["List Students", "Add Student"])
 
 
 # List Students Page
-if page == "Liste des étudiants":
+if page == "List Students":
     # j'affiche la liste des étudiants
     st.title("List of Students")
     st.write("Here you can see the list of students.")
