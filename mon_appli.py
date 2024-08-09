6392bfab1edd2b1ceecf25715c6511f3cf4dbd3e
SomeHenri <nifago@gmail.com>
2024-08-09 20:11:58 +0000

    Update mon_appli.py

--- mon_appli.py
+++ mon_appli.py
@@ -11,8 +11,8 @@ st.write("Bonjour DIT, comment allez vous ? Ici tout va bien par la grâce de Di
 
 # un dataframe
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'first column': [1, 2, 3, 4,5,6,7,8,9,10],
+  'second column': [10, 20, 30, 40,50,60,70,80,90,100]
 })
 
 # affichage du dataframe
